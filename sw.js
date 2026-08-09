55f3cdf1f5808ecec6df216407c208455887bb03
iosifendrodi-ctrl <iosif.endrodi@gmail.com>
2026-08-09 18:18:23 +0300

    Add management order detail view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bertis-order-alert-v5';
+const CACHE='bertis-bertis-order-alert-v6';
 const ASSETS=['/','/index.html','/manifest.json','/icon-192.png','/icon-512.png','/apple-touch-icon.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
